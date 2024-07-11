fea0d39b185ff0db685cbc21db9c9769af89fd3b
guolei <174000902@qq.com>
2024-07-11 11:27:39 +0800

    update

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ class MyTestCase(unittest.TestCase):
     def test_webhook(self):
         webhook = Webhook(
             base_url="https://qyapi.weixin.qq.com/cgi-bin/webhook",
-            key="366095be-225c-4c92-9369-c9d9bc5cc1c5"
+            key=""
         )
         state = webhook.send_text(content="测试发送")
         if state:
