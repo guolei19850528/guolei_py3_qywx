15b469ed74ecf30deab71426a4cf2a15ab65624c
guolei <174000902@qq.com>
2024-07-12 15:40:21 +0800

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name="guolei-py3-qywx",
-    version="0.0.4",
+    version="0.0.5",
     description="企业微信 API",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,9 +15,12 @@ setup(
     author="guolei",
     author_email="174000902@qq.com",
     license="MIT",
-    keywors=["qywx", "weixin", "wechat", "workweixin","企业微信"],
+    keywors=["qywx", "weixin", "wechat", "workweixin", "企业微信"],
     packages=setuptools.find_packages('./'),
     install_requires=[
+        "addict",
+        "retrying",
+        "pydantic",
         "guolei-py3-requests",
         "redis",
         "diskcache"
